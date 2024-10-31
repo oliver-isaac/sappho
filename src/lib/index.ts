f49f7ac123523d060dca2b8c6e7dbffd6ae87ea5
Oliver Isaac <ios.short@me.com>
2024-10-31 19:57:28 +0100

    Move to single file

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,24 @@
-export { sappho as default } from './sappho.ts';
-export { sappho as writer } from './sappho.ts';
-export { reader } from './reader.ts';
+const reader = <T>(value: T) => {
+	const state = $state(value);
+
+	return {
+		get state() {
+			return state;
+		}
+	};
+};
+
+const sappho = <T>(value: T) => {
+	let state = $state(value);
+
+	return {
+		get state() {
+			return state;
+		},
+		set state(val: T) {
+			state = val;
+		}
+	};
+};
+
+export { reader, sappho, sappho as writer };
